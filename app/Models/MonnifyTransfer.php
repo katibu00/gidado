a94cc45a4904f87c16d9d48821926b577e73cd82
katibu01 <ukmisau@gmail.com>
2023-05-25 18:29:19 +0100

    uodate

--- app/Models/MonnifyTransfer.php
+++ app/Models/MonnifyTransfer.php
@@ -21,6 +21,15 @@ class MonnifyTransfer extends Model
         'payment_status',
         'customer_name',
         'customer_email',
+        'bankCode',
+        'amountPaid',
+        'accountName',
+        'sessionId',
+        'accountNumber',
+        'bankName',
+        'name',
+        'email',
+
     ];
     
 }
