b63a522e057add81e68eb234cc9d248495e18da4
katibu01 <ukmisau@gmail.com>
2023-05-25 14:32:34 +0100

    uppdate

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MonnifyController;
 use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +19,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.register');
+   
+    if(auth()->check()){
+        if(Auth::user()->user_type == 'regular'){
+            return redirect()->route('regular.home');
+        }
+    }
+   
+    return view('auth.login');
 });
 
 
 Route::get('/register', [RegisterController::class, 'register'])->name('register');
-// Route::get('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
