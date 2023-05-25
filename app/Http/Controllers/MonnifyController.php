064aaae223cf3a401f1a226b5079c419bfd24653
katibu01 <ukmisau@gmail.com>
2023-05-25 15:37:16 +0100

    update

--- app/Http/Controllers/MonnifyController.php
+++ app/Http/Controllers/MonnifyController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\MonnifyTransfer;
 use Illuminate\Http\Request;
+use App\Models\Wallet;
+use App\Models\ReservedAccount;
 
 class MonnifyController extends Controller
 {
@@ -11,16 +13,32 @@ class MonnifyController extends Controller
     {
         $payload = $request->all();
         $paymentSourceInformation = $payload['eventData']['paymentSourceInformation'][0];
-        
-        MonnifyTransfer::create([
-            'bankCode' => $paymentSourceInformation['bankCode'],
-            'amountPaid' => $paymentSourceInformation['amountPaid'],
-            'accountName' => $paymentSourceInformation['accountName'],
-            'sessionId' => $paymentSourceInformation['sessionId'],
-            'accountNumber' => $paymentSourceInformation['accountNumber'],
-        ]);
-    
-            return response('Webhook received', 200);
+        $amountPaid = $paymentSourceInformation['amountPaid'];
+        $accountNumber = $paymentSourceInformation['accountNumber'];
+
+        // Find the ReservedAccount for the specific user
+        $reservedAccount = ReservedAccount::where('customer_email', $paymentSourceInformation['customer']['email'])->first();
+
+        if ($reservedAccount) {
+            // Retrieve the user's wallet
+            $wallet = Wallet::where('user_id', $reservedAccount->user_id)->first();
+
+            if ($wallet) {
+                $wallet->balance += $amountPaid;
+                $wallet->save();
+                MonnifyTransfer::create([
+                    'bankCode' => $paymentSourceInformation['bankCode'],
+                    'amountPaid' => $amountPaid,
+                    'accountName' => $paymentSourceInformation['accountName'],
+                    'sessionId' => $paymentSourceInformation['sessionId'],
+                    'accountNumber' => $accountNumber,
+                ]);
+
+                return response('Webhook received', 200);
+            }
         }
-    
+
+        return response('Reserved account or wallet not found', 404);
+    }
+
 }
